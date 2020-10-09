d8c94748876f330a2a806bd335f2c5585f2a8237
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-09 17:22:20 -0500

    add react script to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 //export hi
 import { sayHi } from './hi.js';
+//import React
+import React from 'react';
+//import render method
+import { render } from 'react-dom';
 
 //use hi function
 sayHi();
+
+render('Hey there, Im react.js', document.getElementById('app'));
