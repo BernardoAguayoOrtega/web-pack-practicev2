698ff7a08daa2556f043df5a0ca2d4af99692970
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-10-09 20:31:54 -0500

    install babel preset env

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import { render } from 'react-dom';
 //use hi function
 //sayHi();
 
-render(<h1>Hey there, Im react.js</h1>, document.getElementById('app'));
+render(<h1>Hey there, Im react.js :) </h1>, document.getElementById('app'));
